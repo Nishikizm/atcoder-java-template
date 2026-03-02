8a79689b587417042ebd85c27b78cd7ba8bcf6fe
Nishikizm <sakuraiwase649@gmail.com>
2026-03-02 23:31:53 +0900

    Update benchmark

--- src/main/java/io/github/nishikizm/atcoderjavatemplate/benchmark/IOBenchmark.java
+++ src/main/java/io/github/nishikizm/atcoderjavatemplate/benchmark/IOBenchmark.java
@@ -2,10 +2,17 @@ package io.github.nishikizm.atcoderjavatemplate.benchmark;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 import io.github.nishikizm.atcoderjavatemplate.io.BufferedReaderIO;
+import io.github.nishikizm.atcoderjavatemplate.io.InputReader;
 import io.github.nishikizm.atcoderjavatemplate.io.ScannerIO;
 
 public class IOBenchmark {
+
+    @FunctionalInterface
+    private interface IOSupplier<T> {
+        T get(FileInputStream fis) throws IOException;
+    }
 
     @FunctionalInterface
     private interface IOConsumer<T> {
@@ -14,44 +21,54 @@ public class IOBenchmark {
 
     public static void main(String[] args) {
 
-        String intFile = "benchmark-data/generated/int.txt";
-        String longFile = "benchmark-data/generated/long.txt";
-        String doubleFile = "benchmark-data/generated/double.txt";
+        List<String> type = List.of("int", "long", "double");
+        List<IOConsumer<InputReader>> ioConsumers = List.of(
+            reader -> reader.nextInt(), 
+            reader -> reader.nextLong(), 
+            reader -> reader.nextDouble()
+        );
+        List<String> paths = List.of(
+            "benchmark-data/generated/int.txt", 
+            "benchmark-data/generated/long.txt", 
+            "benchmark-data/generated/double.txt"
+        );
         
         try {
-            calculate(sc -> sc.nextInt(), br -> br.nextInt(), intFile);
-            System.out.println();
-            calculate(sc -> sc.nextLong(), br -> br.nextLong(), longFile);
-            System.out.println();
-            calculate(sc -> sc.nextDouble(), br -> br.nextDouble(), doubleFile);
-            System.out.println();
+            runBenchMark(type, ioConsumers, paths);
         } catch(IOException e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
     }
 
-    private static void calculate(IOConsumer<ScannerIO> slow, 
-                                    IOConsumer<BufferedReaderIO> fast, 
-                                    String path) throws IOException {
-        try(FileInputStream sFis = new FileInputStream(path)) {
-            ScannerIO sc = new ScannerIO(sFis);
+    private static void runBenchMark(
+        List<String> type, 
+        List<IOConsumer<InputReader>> ioConsumers, 
+        List<String> paths) throws IOException {
 
-            long scStart = System.nanoTime();
-            slow.accept(sc);
-            long scEnd = System.nanoTime();
-            long scDiff = scEnd - scStart;
-            System.out.print("Scanner: " + (scDiff / 1_000_000) + "ms ");
+        for(int i = 0; i < type.size(); i++) {
+            System.out.println("=== " + type.get(i) + " ===");
+            for(int j = 0; j < 5; j++) {
+                System.out.print((j + 1) + ": ");
+                calculate(ScannerIO::new, ioConsumers.get(i), paths.get(i), "Scanner");
+                calculate(BufferedReaderIO::new, ioConsumers.get(i), paths.get(i), "BufferedReader");
+                System.out.println();
+            }
         }
+    }
 
-        try(FileInputStream bFis = new FileInputStream(path)) {
-            BufferedReaderIO br = new BufferedReaderIO(bFis);
+    private static <T extends InputReader> void calculate(
+        IOSupplier<T> supplyReader, IOConsumer<T> read, String path, String type) throws IOException {
+            
+        try(FileInputStream fis = new FileInputStream(path)) {
+            T reader = supplyReader.get(fis);
 
-            long brStart = System.nanoTime();
-            long brEnd = System.nanoTime();
-            fast.accept(br);
-            long brDiff = brEnd - brStart;
-            System.out.print("BufferedReader: " + (brDiff / 1_000_000) + "ms ");
+            long start = System.nanoTime();
+            read.accept(reader);
+            long end = System.nanoTime();
+
+            long diff = end - start;
+            System.out.print(type + ": " + (diff / 1_000_000) + "ms  ");
         }
     }
 }
